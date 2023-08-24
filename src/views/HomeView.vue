eedc9465012d252d4af744828a7f295311b77ef9
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-08-24 10:13:34 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Hotel finder</h1>
   <p>
-    
+    In this web application you can find hotels, based on their location relative to my position zero (Åssiden, Drammen) <br>
+    I will not include every single one, but a selected array.
   </p>
 </template>
 
