29b47b6da3ff1637b7609c4e9226f6879be0e929
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-08-24 14:08:47 +0200

    fgh

--- src/components/FilterPanel.vue
+++ src/components/FilterPanel.vue
@@ -1,12 +1,24 @@
 <template>
 <form>
-    <input id="distance" type="range" :min=20 :max=100000><br>
-    <label for="distance">Distance: </label>
+    <input 
+    id="distance" 
+    type="range" 
+    min="20" 
+    max="2000"
+    step="10"
+    v-model="selectedDistance"
+    style="width: 500px;"
+    /><br>
+    <label for="distance">Distance from Drammen: {{ this.selectedDistance }}km </label>
 </form>
 </template>
 
 <script>
 export default{
-
+    data() {
+        return{
+            selectedDistance: 500,
+        }
+    }
 }
 </script>
