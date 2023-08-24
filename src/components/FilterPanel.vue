b7c77ff3a0f33eed89731e9981ec59821cff7b9a
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-08-24 13:14:03 +0200

    Update FilterPanel.vue

--- src/components/FilterPanel.vue
+++ src/components/FilterPanel.vue
@@ -1,3 +1,12 @@
 <template>
-    
+<form>
+    <input id="distance" type="range" :min=20 :max=100000><br>
+    <label for="distance">Distance: </label>
+</form>
 </template>
+
+<script>
+export default{
+
+}
+</script>
